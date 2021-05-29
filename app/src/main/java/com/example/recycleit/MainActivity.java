5ae6370b9841b96fcb1bbf85907d1475a544203d
nco0978 <xxnikoxxskate@gmail.com>
2021-05-29 01:51:49 -0400

    PopUpPerfil

--- app/src/main/java/com/example/recycleit/MainActivity.java
+++ app/src/main/java/com/example/recycleit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.recycleit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -7,11 +8,16 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     FragmentTransaction transaction;
     Fragment mainFragment;
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button btnAtras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +33,29 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.item1) {
             Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show();
+            createNewContacDialog();
 
         }
         return super.onOptionsItemSelected(item);
+
+    }
+    //Popup Menu
+    public void createNewContacDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View contactPopupView = getLayoutInflater().inflate(R.layout.popupperfil,null);
+        //Codigo layout popup
+        btnAtras = (Button) contactPopupView.findViewById(R.id.btnAtras);
+        //Codigo layout popup
+        dialogBuilder.setView(contactPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+        //botones
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
     }
 }
