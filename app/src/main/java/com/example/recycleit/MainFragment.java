69cbdaa73036b60451f6700fd94f3d401ac13e76
nco0978 <xxnikoxxskate@gmail.com>
2021-06-15 04:52:12 -0400

    Modificación snippet.

--- app/src/main/java/com/example/recycleit/MainFragment.java
+++ app/src/main/java/com/example/recycleit/MainFragment.java
@@ -21,14 +21,18 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.math.BigInteger;
 
 public class MainFragment extends Fragment implements OnMapReadyCallback {
 
@@ -75,16 +79,16 @@ public class MainFragment extends Fragment implements OnMapReadyCallback {
         map = googleMap;
 
 
-        LatLng restaurantSoju = new LatLng(-33.4381297,-70.6487444);
-        map.addMarker(new MarkerOptions().position(restaurantSoju).title("Centro de reciclaje Plaza de Armas Merced 821 Santiago").snippet("plástico-carton-vidrio-aluminio"+"hola").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
+        LatLng recoclajePlazaDA = new LatLng(-33.4381297,-70.6487444);
+        map.addMarker(new MarkerOptions().position(recoclajePlazaDA).title("Centro de reciclaje Plaza de Armas Merced 821 Santiago").snippet("Plástico/Carton/Vidrio/aluminio/.").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
         LatLng reciclajeReyes = new LatLng(-33.428412526129215,-70.66830728225443);
-        map.addMarker(new MarkerOptions().position(reciclajeReyes).title("Punto Limpio Parque de Los Reyes").snippet("Dirección: Santiago, Región Metropolitana").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
+        map.addMarker(new MarkerOptions().position(reciclajeReyes).title("Punto Limpio Parque de Los Reyes").snippet("Plástico/Carton/Vidrio/Aceite/.").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
         LatLng reciclajeCerrillos = new LatLng(-33.5051505,-70.728599);
-        map.addMarker(new MarkerOptions().position(reciclajeCerrillos).title("Punto Limpio TriCiclos - Sodimac Cerrillos").snippet("Dirección: Avenida Américo Vespucio 1501, Cerrillos Estacionamiento Homecenter Sodimac Mall Plaza Oeste, Santiago, Región Metropolitana").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
+        map.addMarker(new MarkerOptions().position(reciclajeCerrillos).title("Punto Limpio TriCiclos - Sodimac Cerrillos").snippet("Plástico/Carton/Vidrio/Aceite/Electrico/.").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
         LatLng reciclajeElBosque = new LatLng(-33.5623518,-70.6768486);
-        map.addMarker(new MarkerOptions().position(reciclajeElBosque).title("Punto Limpio TriCiclos - Sodimac El Bosque").snippet("Dirección: Av José Miguel Carrera 10375, El Bosque Estacionamiento Homecenter Sodimac Open Plaza, El Bosque, Santiago, Región Metropolitana").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
+        map.addMarker(new MarkerOptions().position(reciclajeElBosque).title("Punto Limpio TriCiclos - Sodimac El Bosque").snippet("Plástico/Carton/Vidrio/Aceite/Electrónica/Orgánico/.").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
         LatLng reciclajeSanJoaquin = new LatLng(-33.5,-70.61667);
-        map.addMarker(new MarkerOptions().position(reciclajeSanJoaquin).title("Punto de reciclaje").snippet("Dirección: Uno de Septiembre 308, San Joaquín, Región Metropolitana").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
+        map.addMarker(new MarkerOptions().position(reciclajeSanJoaquin).title("Punto de Reciclaje - Uno de Septiembre 308, San Joaquín").snippet("Plástico/Carton/Vidrio/Aceite/Electrónica/.").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntoreciclajeextraextrachico)));
         //Centrar Mapa
         //map.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurantSoju, 11));
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -98,15 +102,15 @@ public class MainFragment extends Fragment implements OnMapReadyCallback {
             return;
         }
         map.setMyLocationEnabled(true);
-
         map.getUiSettings().setMyLocationButtonEnabled(true);
-
         LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 LatLng miUbicacion = new LatLng(location.getLatitude(), location.getLongitude());
+                map.addMarker(new MarkerOptions().position(miUbicacion).title("ubicacion actual"));
                 //map.addMarker(new MarkerOptions().position(miUbicacion).title("ubicacion actual"));
+                //map.moveCamera(CameraUpdateFactory.newLatLngZoom(reciclajeReyes, 11));
 
             }
 
